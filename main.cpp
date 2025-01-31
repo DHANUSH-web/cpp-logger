16d2a60bb97a776825ad0a81e1578258e12ddda6
Dhanush H V <dhanushhv75@gmail.com>
2025-02-01 02:19:54 +0530

    Updated the main file with comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+/*
+ * Use this file to demo or practive Logger usage and implementation.
+ * Please contribute to this project by raising pull request at
+ * https://github.com/DHANUSH-web/cpp-logger.git/pulls
+ *
+ * Developer  - Dhanush H V <dhanushhv75@gmail.com>
+ * Website    - https://dhanushhv.vercel.app
+ */
+
 #include <logger.h>
 
 int main() {
-    const std::string root_dir = "cache";
-    const std::string log_file = "test_logger.log";
+    const std::string root_dir = "";    // Insert absolute path of log file root
+    const std::string log_file = "";    // Insert log file name
 
     // Initiated logger instance
     auto logger = Logger(root_dir, log_file);
