0c89381cb3e8d06daa3590fce0a937211dc776ff
DHANUSH H V <dhanushhv75@gmail.com>
2025-02-11 01:50:41 +0530

    Fix: Replaced gcc with clang for handling file systems properly

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@
 REM Set the path variables
 set BUILD_DIR=out
 set EXECUTABLE="cpplogger.exe"
-set GPP="g++.exe"
+set GCC="clang.exe"
+set GPP="clang++.exe"
 set CMAKE="cmake.exe"
 set MAKE="mingw32-make.exe"
 set GENERATOR="MinGW Makefiles"
@@ -18,17 +19,16 @@ REM Change directory to build directory
 cd %BUILD_DIR%
 
 REM Compile the project with CMake gcc and g++ compilers
-%CMAKE% -G %GENERATOR% -DCMAKE_CXX_COMPILER=%GPP% -DCMAKE_MAKE_PROGRAM=%MAKE% ..
+%CMAKE% -G %GENERATOR% -DCMAKE_C_COMPILER=%GCC% -DCMAKE_CXX_COMPILER=%GPP% -DCMAKE_MAKE_PROGRAM=%MAKE% ..
 
 REM Build the Makefile
-REM %CMAKE% --build .
-%MAKE%
-
-REM Execute the final binary
-%EXECUTABLE%
+%CMAKE% --build .
 
 REM Remove the final binary once executed
 cd ..
+
+REM Execute the final binary
+"./%BUILD_DIR%/%EXECUTABLE%"
 
 REM Print the final success message for little nerd happiness after hours of grinding :)
 echo "You did it nerd.. Congrats!! Now go and have a break buddy :)"
